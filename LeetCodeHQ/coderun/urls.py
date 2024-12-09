03a972b052a0f62fc0f331d1fb0368cfbedc4c76
Ali <79370758+MHM-Rajpoot@users.noreply.github.com>
2024-12-08 21:50:41 -0800

    Add files via upload

--- LeetCodeHQ/coderun/urls.py
+++ LeetCodeHQ/coderun/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.indexc, name='indexc'),
-    path('run_code/', views.run_code, name='run_code'),
+    path('coding/', views.coding, name='coding'),
 ]
